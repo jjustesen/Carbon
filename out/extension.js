597e5fac2458eb3065195b846eaa807c2111fc8f
jjustesen <johannesjustensen99@gmail.com>
2024-07-27 12:41:52 -0300

    chore: Update file tree data provider to refresh on active text editor change and add command to open file

--- out/extension.js
+++ out/extension.js
@@ -31,6 +31,10 @@ function activate(context) {
     console.log('A extensão "minha-extensao" está ativa!');
     const treeDataProvider = createFileTreeDataProvider();
     vscode.window.registerTreeDataProvider("fileExplorer", treeDataProvider);
+    // Registra o comando para abrir o arquivo
+    vscode.commands.registerCommand("fileExplorer.openFile", (filePath) => {
+        openFile(filePath);
+    });
     // Atualiza a árvore quando o arquivo aberto mudar
     vscode.window.onDidChangeActiveTextEditor((editor) => {
         if (editor) {
@@ -52,7 +56,21 @@ function createFileTreeDataProvider() {
             const editor = vscode.window.activeTextEditor;
             if (editor) {
                 const filePath = editor.document.fileName;
-                const importers = await findImporters(filePath);
+                const fileName = path.basename(filePath, path.extname(filePath));
+                //Encontra as formas de exportação do arquivo
+                await vscode.workspace.fs
+                    .readFile(vscode.Uri.file(filePath))
+                    .then((content) => {
+                    console.log(filePath);
+                    const text = content.toString();
+                    //separa exportações default e export em const
+                    const exportRegexDefault = new RegExp(`export\\s+(default\\s+)?(function|class|const|let|var)\\s+${fileName}`);
+                    const exportRegexConst = new RegExp(`export\\s+const\\s+${fileName}`);
+                    const exportRegexFunction = new RegExp(`export\\s+function\\s+${fileName}`);
+                    const exportRegexClass = new RegExp(`export\\s+class\\s+${fileName}`);
+                    const exportDefault = exportRegexDefault.test(text);
+                });
+                const importers = await findImporters(fileName);
                 return Promise.resolve(importers.map((importer) => createFileItem(importer)));
             }
             else {
@@ -76,33 +94,38 @@ function createFileItem(filePath) {
         description: filePath,
         filePath: filePath,
         collapsibleState: vscode.TreeItemCollapsibleState.None,
+        command: {
+            command: "fileExplorer.openFile",
+            title: "Open File",
+            arguments: [filePath],
+        },
     };
     return item;
 }
-async function findImporters(filePath) {
+async function findImporters(fileName) {
     const workspaceFolders = vscode.workspace.workspaceFolders;
     if (!workspaceFolders) {
         return [];
     }
     const importers = [];
     for (const folder of workspaceFolders) {
-        const files = await vscode.workspace.findFiles(new vscode.RelativePattern(folder, "**/*.tsx"), "**/node_modules/**");
+        const files = await vscode.workspace.findFiles(new vscode.RelativePattern(folder, "**/*.{js,ts,jsx,tsx,vue}"), "**/node_modules/**");
         for (const file of files) {
             const content = await vscode.workspace.fs.readFile(file);
             const text = content.toString();
-            const relativeFilePath = path
-                .relative(folder.uri.fsPath, filePath)
-                .replace(/\\/g, "/");
-            const fileName = path.basename(relativeFilePath, ".tsx");
-            console.log(fileName);
+            // Atualizar a regex para capturar importações que incluam o nome do arquivo
             const importRegex = new RegExp(`import\\s+.*\\s+from\\s+['"].*${fileName}['"];?`);
-            // Use o regex para encontrar importações que terminam com o nome do arquivo
-            // const matches = sourceCode.match(importRegex);
+            const matches = text.match(importRegex);
+            console.log(matches);
             if (importRegex.test(text)) {
                 importers.push(file.fsPath);
             }
         }
     }
     return importers;
+}
+async function openFile(filePath) {
+    const document = await vscode.workspace.openTextDocument(filePath);
+    await vscode.window.showTextDocument(document);
 }
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,19 @@
 import * as vscode from "vscode";
 import * as path from "path";
-import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('A extensão "minha-extensao" está ativa!');
 
   const treeDataProvider = createFileTreeDataProvider();
   vscode.window.registerTreeDataProvider("fileExplorer", treeDataProvider);
+
+  // Registra o comando para abrir o arquivo
+  vscode.commands.registerCommand(
+    "fileExplorer.openFile",
+    (filePath: string) => {
+      openFile(filePath);
+    }
+  );
 
   // Atualiza a árvore quando o arquivo aberto mudar
   vscode.window.onDidChangeActiveTextEditor((editor) => {
@@ -34,7 +41,33 @@ function createFileTreeDataProvider(): vscode.TreeDataProvider<FileItem> {
       const editor = vscode.window.activeTextEditor;
       if (editor) {
         const filePath = editor.document.fileName;
-        const importers = await findImporters(filePath);
+        const fileName = path.basename(filePath, path.extname(filePath));
+
+        //Encontra as formas de exportação do arquivo
+        await vscode.workspace.fs
+          .readFile(vscode.Uri.file(filePath))
+          .then((content) => {
+            console.log(filePath);
+            const text = content.toString();
+
+            //separa exportações default e export em const
+            const exportRegexDefault = new RegExp(
+              `export\\s+(default\\s+)?(function|class|const|let|var)\\s+${fileName}`
+            );
+            const exportRegexConst = new RegExp(
+              `export\\s+const\\s+${fileName}`
+            );
+            const exportRegexFunction = new RegExp(
+              `export\\s+function\\s+${fileName}`
+            );
+            const exportRegexClass = new RegExp(
+              `export\\s+class\\s+${fileName}`
+            );
+
+            const exportDefault = exportRegexDefault.test(text);
+          });
+
+        const importers = await findImporters(fileName);
         return Promise.resolve(
           importers.map((importer) => createFileItem(importer))
         );
@@ -65,13 +98,19 @@ function createFileItem(filePath: string): FileItem {
     description: filePath,
     filePath: filePath,
     collapsibleState: vscode.TreeItemCollapsibleState.None,
+    command: {
+      command: "fileExplorer.openFile",
+      title: "Open File",
+      arguments: [filePath],
+    },
   };
 
   return item;
 }
 
-async function findImporters(filePath: string): Promise<string[]> {
+async function findImporters(fileName: string): Promise<string[]> {
   const workspaceFolders = vscode.workspace.workspaceFolders;
+
   if (!workspaceFolders) {
     return [];
   }
@@ -79,25 +118,20 @@ async function findImporters(filePath: string): Promise<string[]> {
   const importers: string[] = [];
   for (const folder of workspaceFolders) {
     const files = await vscode.workspace.findFiles(
-      new vscode.RelativePattern(folder, "**/*.tsx"),
+      new vscode.RelativePattern(folder, "**/*.{js,ts,jsx,tsx,vue}"),
       "**/node_modules/**"
     );
 
     for (const file of files) {
       const content = await vscode.workspace.fs.readFile(file);
       const text = content.toString();
-      const relativeFilePath = path
-        .relative(folder.uri.fsPath, filePath)
-        .replace(/\\/g, "/");
-      const fileName = path.basename(relativeFilePath, ".tsx");
-      console.log(fileName);
 
+      // Atualizar a regex para capturar importações que incluam o nome do arquivo
       const importRegex = new RegExp(
         `import\\s+.*\\s+from\\s+['"].*${fileName}['"];?`
       );
-
-      // Use o regex para encontrar importações que terminam com o nome do arquivo
-      // const matches = sourceCode.match(importRegex);
+      const matches = text.match(importRegex);
+      console.log(matches);
       if (importRegex.test(text)) {
         importers.push(file.fsPath);
       }
@@ -105,4 +139,9 @@ async function findImporters(filePath: string): Promise<string[]> {
   }
 
   return importers;
+}
+
+async function openFile(filePath: string) {
+  const document = await vscode.workspace.openTextDocument(filePath);
+  await vscode.window.showTextDocument(document);
 }
